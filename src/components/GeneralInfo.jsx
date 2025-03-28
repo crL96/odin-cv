fa976ef42eac3aec2345539041fb0a019d59e5c2
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-28 11:08:54 +0100

    Add GeneralInfo component

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,17 @@
+export default function GeneralInfo() {
+
+    return (
+        <fieldset id="generalInfo">
+            <legend>General Info</legend>
+            <label>Name:
+                <input type="text" name="name" id="name" placeholder="John Smith"/>
+            </label>
+            <label>Email: 
+                <input type="email" name="email" id="email" />
+            </label>
+            <label>Phone: 
+                <input type="tel" id="phone" name="phone"/>
+            </label>
+        </fieldset>
+    );
+}
