1e94ff0efc8744ecd99712fb4fcfeb102aace605
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-28 11:20:53 +0100

    Add together components for a full form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import "./App.css";
+import GeneralInfo from "./components/GeneralInfo";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+
 
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return (
+        <>
+            <h1>CV Application</h1>
+            <form action="" method="post" id="cvForm">
+                <GeneralInfo />
+                <Education />
+                <Experience />
+                <button>Submit</button>
+            </form>
+        </>
+    );
 }
