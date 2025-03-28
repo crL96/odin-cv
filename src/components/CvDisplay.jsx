801a1ba165429737c173cdb76e35ced411088106
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-28 12:19:14 +0100

    Add CvDisplay component that displays user input in CV format

---
+++ src/components/CvDisplay.jsx
@@ -0,0 +1,25 @@
+export default function CvDisplay({userData}) {
+    return (
+        <div id="cvDisplay">
+            <div id="displayGeneral">
+                <h2>General Info</h2>
+                <h3>{userData.name}</h3>
+                <p>{userData.email}</p>
+                <p>{userData.phone}</p>
+            </div>
+            <div id="displayEducation">
+                <h2>Education</h2>
+                <h3>{userData.study}</h3>
+                <h4>{userData.school}</h4>
+                <p>{userData.startDateEdu} - {userData.endDateEdu}</p>
+            </div>
+            <div id="displayExperience">
+                <h2>Practical Experience</h2>
+                <h3>{userData.jobTitle}</h3>
+                <h4>{userData.company}</h4>
+                <p>{userData.responsibility}</p>
+                <p>{userData.startDateJob} - {userData.endDateJob}</p>
+            </div>
+        </div>
+    );
+}
