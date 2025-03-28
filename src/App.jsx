6499eb2ed9f53b0b5981bb8c4359418968d51503
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-28 13:38:40 +0100

    Remove h1 header

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,6 @@ export default function App() {
 
   return (
         <>
-            <h1>CV Application</h1>
             <form action="" method="post" id="cvForm" onSubmit={handleSubmit} hidden={hiddenStatus}>
                 <GeneralInfo />
                 <Education />
