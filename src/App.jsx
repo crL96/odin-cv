578341bba283f288d44e2645d58a6e7e92294f60
crL96 <190506813+crL96@users.noreply.github.com>
2025-03-28 12:20:13 +0100

    Add functions for handling submit and edit buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import GeneralInfo from "./components/GeneralInfo";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+import CvDisplay from "./components/CvDisplay";
 
 
 export default function App() {
+  const [hiddenStatus, setHiddenStatus] = useState(false);
+  const [userData, setUserData] = useState({});
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      setUserData(Object.fromEntries(formData));
+      setHiddenStatus(true);
+  }
+
+  function handleEdit() {
+    setHiddenStatus(false);
+  }
+
+
   return (
         <>
             <h1>CV Application</h1>
-            <form action="" method="post" id="cvForm">
+            <form action="" method="post" id="cvForm" onSubmit={handleSubmit} hidden={hiddenStatus}>
                 <GeneralInfo />
                 <Education />
                 <Experience />
                 <button>Submit</button>
             </form>
+            {hiddenStatus && 
+            <>
+              <CvDisplay userData={userData}/>
+              <button id="editBtn" type="button" onClick={handleEdit}>Edit</button>
+            </>}
         </>
     );
 }
